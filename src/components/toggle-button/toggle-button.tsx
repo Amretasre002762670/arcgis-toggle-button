a038042d09bca74bd91df4ee06f5a2d66b7b52af
Amretasre002762670 <amr14649@esri.com>
2025-06-17 13:37:27 -0700

    changed ex to %

--- src/components/toggle-button/toggle-button.tsx
+++ src/components/toggle-button/toggle-button.tsx
@@ -1,4 +1,5 @@
 import { LitElement, state, h, type JsxNode } from "@arcgis/lumina";
+import { styles } from "./toggle-button.scss";
 
 declare global {
   interface DeclareElements {
@@ -7,9 +8,17 @@ declare global {
 }
 
 export class ToggleButton extends LitElement {
+
+  //#region Static Properties
+
+  static override styles = styles;
+
+  //#endregion
+
   //#region State Properties
-    @state()
-    on = false;
+
+  @state()
+  on = false;
 
   //#endregion
 
@@ -17,15 +26,32 @@ export class ToggleButton extends LitElement {
 
   override render(): JsxNode {
     return (
-        <button
-            
-            onClick={() => {
-            this.on = !this.on;
-            }}
-        >
-            {this.on ? "On" : "Off"}
-        </button>
+        <div class={`flip-toggle${this.on ? " on" : ""}`} onClick={this.handleToggle} onKeyDown={this.handleToggleKeyPress} tabIndex={1} role="switch" aria-label="Toggle Button" aria-checked={this.on}>
+          <div class="knob"></div>
+          <span class="label on-label">On</span>
+          <span class="label off-label">Off</span>
+        </div>
     );
+  }
+
+  //#endregion
+
+  //#region Events
+
+  private handleToggle(): void {
+    this.on = !this.on;
+    this.el.dispatchEvent(new CustomEvent("toggle", {
+      detail: { on: this.on },
+      bubbles: true,
+      composed: true
+    }));
+  }
+
+  private handleToggleKeyPress(event: KeyboardEvent) :void {
+    if (event.key === "Enter" || event.key === " ") {
+      this.handleToggle();
+      event.preventDefault();
+    }
   }
 
   //#endregion
